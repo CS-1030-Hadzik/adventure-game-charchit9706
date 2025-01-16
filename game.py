606e6e3972330b9e59640ed88a3f5cc8e7e6fc32
Charchit Pokhrel <charchitpokhrel@mail.weber.edu>
2025-01-16 10:05:03 -0700

    ADD:Doc String

--- game.py
+++ game.py
@@ -1,2 +1,13 @@
+# Welcome messag and introduction
+'''
+Adventure Game
+Author: Charchit Pokhrel
+Version: 1.0
+Description:
+This is a text-based adventure game where the player makes choices
+to navigate through a mysterious forest
+'''
+
+
 print("Welcome to the Adventure Game!")  
 print("Your journey begins here...")
